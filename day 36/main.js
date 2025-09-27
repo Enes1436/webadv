fa3ec17f8a073d1bbbb00fb4fd43e400e5558571
Enes1436 <enesmenxhiqi079@gmail.com>
2025-09-27 05:49:41 -0700

    .......

---
+++ day 36/main.js
@@ -0,0 +1,23 @@
+function openMenu(){
+    var menu=document.getElementById("links");
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "block";
+    }
+}
+
+function search(){
+    var input , filter, ul, li, a, txtvalue;
+    input= document.getElementById("myInput")
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myList");
+    li = ul.getElementsByTagName("li");
+    
+    for (var i = 0; i < li.length; i++) 
+        a = li[i].getElementsByTagName("a")[0];
+        txtvalue = a.textContent || a.innerText;
+        if (txtvalue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "none";
+        } else 
+}
