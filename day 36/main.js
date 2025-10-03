e48ee8c7bac376cdc03e7b7ce00e8fca6298fcd5
Enes1436 <enesmenxhiqi079@gmail.com>
2025-10-03 19:18:03 +0200

    ......................

--- day 36/main.js
+++ day 36/main.js
@@ -8,16 +8,19 @@ function openMenu(){
 }
 
 function search(){
-    var input , filter, ul, li, a, txtvalue;
-    input= document.getElementById("myInput")
-    filter = input.value.toUpperCase();
+    var input, filtre, ul, li, a , i, txtvalue;
+    input=document.getElementById("myInput");
+    filter = input.ariaValueMax.toUpperCase();
     ul = document.getElementById("myList");
-    li = ul.getElementsByTagName("li");
-    
-    for (var i = 0; i < li.length; i++) 
-        a = li[i].getElementsByTagName("a")[0];
+li=ul.getElementsByTagName("li");
+
+    for(i=0; i<li.length; i++){
+        a = li[i];
         txtvalue = a.textContent || a.innerText;
-        if (txtvalue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "none";
-        } else 
+        if(txtvalue.toUpperCase().indexOf(filtre)>-1){
+            li[i].style.display = "block";
+        }else{
+            li[i].style.display = "none"
+        }
+    }
 }
